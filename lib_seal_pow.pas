7e0f01afd60a79133b3db71be1efd2d3e00303be
Mark Bishop <github.sealfin@sealfin.com>
2013-10-29 11:09:48 +0000

    Update lib_seal_pow.pas

--- lib_seal_pow.pas
+++ lib_seal_pow.pas
@@ -2,20 +2,23 @@ unit lib_seal_pow;
 
 interface
 
-function f_Pow( p_base, p_exponent : integer ) : integer;
+function f_Pow( p_base, p_exponent : integer ) : real;
 
 implementation
 
-function f_Pow( p_base, p_exponent : integer ) : integer;
+function f_Pow( p_base, p_exponent : integer ) : real;
 var
-  result, i : integer;
+  result : real;
+  i : integer;
 begin
   if p_exponent = 0 then
     result := 1
   else begin
     result := p_base;
-    for i := 1 to p_exponent - 1 do
-      result := result * p_base
+    for i := 1 to abs( p_exponent ) - 1 do
+      result := result * p_base;
+    if p_exponent < 0 then
+      result := 1 / result
   end;
   f_Pow := result
 end;
